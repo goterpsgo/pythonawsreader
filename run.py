3b7a4a0137e710746e3f5c1b4286a5946cc7582f
Joe Tseng <jtsengorg@gmail.com>
2017-10-16 04:53:43 +0000

    Uses boto3 to retrieve EC2 instances data. NOTE: still need to get AWS to accept keys for 3rd party auth.

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-import os
+#!/usr/bin/env python
+
 import json
+import boto3
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__,
@@ -8,6 +11,11 @@ app = Flask(__name__,
 
 app.config.from_pyfile("./config.py")
 
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
+
 @app.route("/", methods=["GET"])
 def index():
 	return render_template("index.html")
@@ -15,10 +23,17 @@ def index():
 @app.route("/resultsdata", methods=["GET", "POST"])
 def results():
 	dummy_data = ['foo', {'bar': ('baz', None, 1.0, 2)}]
+	# print boto3.Session().get_credentials().access_key
+	# print boto3.Session().get_credentials().secret_key
 
 	if request.method == "POST":
-		print "Request data: %r" % request.data
-		return request.data
+
+		data = json.loads(request.data)
+
+		# ec2 = boto3.client("ec2", region_name="us-east-1", aws_access_key_id=str(data["access_key_id"]), aws_secret_access_key=str(data["secret_access_key"]))
+		ec2 = boto3.client("ec2", region_name="us-east-1")
+		results = ec2.describe_instances()
+		return json.dumps(results, default=datetime_handler)
 	else:
 		return json.dumps(dummy_data) 
 
