7f87932f9f041235ef846d4ae2f968cc911947ca
Joe Tseng <jtsengorg@gmail.com>
2017-10-16 14:27:32 +0000

    Codebase was pasted twice in file.

--- static/js/controllers/results.controller.js
+++ static/js/controllers/results.controller.js
@@ -8,46 +8,6 @@
     function Controller($scope, ResultsService) {
 
         var vm = this;
-        vm.this_form = {};
-        vm.results = [];
-
-        initController();
-
-        function initController() {
-            get_results();
-        };
-
-        vm.post_results = function(_form) {
-            console.log('Running post_results(): ');
-            console.log(_form);
-            // ResultsService._post_result()
-            //     .then(
-            //         function(results) {
-            //             console.log('Posted results');
-            //         }
-            //     )
-        }
-
-        function get_results() {
-            ResultsService._get_results()
-                .then(
-                    function(results) {
-                        console.log(results);
-                    }
-                );
-        }
-    }
-})();
-(function () {
-    'use strict';
- 
-    angular
-        .module('app')
-        .controller('ResultsController', Controller);
-
-    function Controller($scope, ResultsService) {
-
-        var vm = this;
         $scope.this_form = {};
         $scope.results = [];
 
